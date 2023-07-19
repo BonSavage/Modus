88f4a3d619c1b7ce23a03c17e9f61960b40df1eb
BonSavage <mkirillov8402@gmail.com>
2023-07-19 13:12:44 +0300

    Added debug utilities and minor fix of recursion block

--- modus.lisp
+++ modus.lisp
@@ -1,7 +1,7 @@
 ;;;; Modus: Prolog-like language interpreter
-;;;; 14.07.2022
+;;;; 09.07.2023
 ;;;; Maxim "BonSavage" Kirillov
-;;;; Todo: effecient compiler
+;;;; Todo: efficient compiler
 
 (in-package :modus)
 
@@ -60,6 +60,10 @@
 
 ;;Evaluator
 
+(defvar *sweeped* nil)
+(defvar *deepness* 0)
+(defvar *sweeped-rules* nil)
+
 (defun extend-bindings(bnds1 bnds2)
   (append bnds1 bnds2))
 
@@ -92,29 +96,29 @@
   (eval-or-iter (mapcar #'expr-predicate body) nil))
 
 (defun eval-none(pattern)
-  (mvb (_ succ) (eval-pattern (expr-predicate pattern))
-       (unless (or _ succ)
+  (mvb (bnds succ) (eval-pattern (expr-predicate pattern))
+       (unless (or bnds succ)
 	 (values (bindings-list nil) t))))
   
 (defun eval-pattern(pattern)
-  (if
-   (sweepedp pattern)
-   nil
-   (let-be [*sweeped* (cons pattern *sweeped*)]
-     (if (atom (predicate-body pattern))
-	 (rule-inference pattern)
-	 (destructuring-bind (op &rest args) (predicate-body pattern)
-	   (case op
-	     (and (with-subs subs (eval-and args)
-		    subs))
-	     (or (with-subs subs (eval-or args)
-		   subs))
-	     (none (apply #'eval-none args))
-	     (lisp-value (apply (car args) (cdr args)))
-	     (t (with-subs subs (rule-inference pattern)
-		  subs))))))))
-
-(defvar *sweeped* nil)
+  (cond ((sweepedp pattern) 
+	 nil)
+	((= *deepness* 30) (error "Modus: it looks like program entered infinite recursion. Evaluation aborted due to recursion became too big.~&Last pattern is:~& ~a" pattern))
+	(t
+	 (let-be [*sweeped* (cons pattern *sweeped*)
+		  *deepness* (1+ *deepness*)]
+	   (if (atom (predicate-body pattern))
+	       (rule-inference pattern)
+	       (destructuring-bind (op &rest args) (predicate-body pattern)
+		 (case op
+		   (and (with-subs subs (eval-and args)
+			  subs))
+		   (or (with-subs subs (eval-or args)
+			 subs))
+		   (none (apply #'eval-none args))
+		   (lisp-value (apply (car args) (cdr args)))
+		   (t (with-subs subs (rule-inference pattern)
+			subs)))))))))
 
 (defun find-known(pattern)
   (awith (filtering-map (lambda (known)
@@ -129,7 +133,11 @@
     (intersection subs (predicate-args pred1) :test (lambda (bnd arg) (eq arg (car bnd))))))
 
 (defun sweepedp(pattern)
-  (find pattern *sweeped* :test (lambda (pred swpd) (partialp (predicate-body pred) (predicate-body swpd)))))
+  (find pattern *sweeped*
+	:test
+	(lambda (pred swpd)
+	  (and (eql (first (predicate-body pred)) (first (predicate-body swpd)))
+	       (partialp (predicate-body pred) (predicate-body swpd))))))
 
 
 ;; Inference itself
@@ -137,16 +145,19 @@
   (filtering-map (lambda (rule) (with-subs subs (unify-predicates* (rule-consequence rule) pattern)
 				  (cons subs (make-rule (predicate-sublis subs (rule-prerequisite rule))
 							(predicate-sublis subs (rule-consequence rule))))))
-		 *rules*))
+		 (remove-if (lambda (rule) (not (eql (first (predicate-body (rule-consequence rule)))
+						     (first (predicate-body pattern)))))
+			    *rules*)))
 
 (defun rules-bindings(pattern filtered-rules)
   (mappend (lambda (pair &aux (subed (predicate-sublis (car pair) (rule-prerequisite (cdr pair)))))
-	     (with-subs it (eval-pattern subed)
-	       (or (mapcar (lambda (subs) (unify-predicates*
-					   pattern
-					   (predicate-sublis (extend-bindings (car pair) subs) (rule-consequence (cdr pair)))))
-			   it)
-		   (list (bindings-list nil)))))
+	     (let-be [*sweeped-rules* (cons pair *sweeped-rules*)]
+	       (with-subs it (eval-pattern subed)
+		 (or (mapcar (lambda (subs) (unify-predicates*
+					     pattern
+					     (predicate-sublis (extend-bindings (car pair) subs) (rule-consequence (cdr pair)))))
+			     it)
+		     (list (bindings-list nil))))))
 	   filtered-rules))
 
 (defun rule-inference(pattern)
@@ -163,12 +174,6 @@
 	     (aif (rules-bindings pattern rules)
 		  (values (or it (bindings-list nil)) t))))
 	  (values it t)))))
-  
-(defun infsub(pattern)
-  (mvb (subs succ) (rule-inference pattern)
-       (if succ
-	   (mapcar (lambda (sub) (predicate-sublis sub pattern)) subs)
-	   nil)))
 
 (defun evaluate(expr)
   (eval-pattern (expr-predicate expr)))
